cbd68bc5b980dabea9a0e7c7c409f8e64e3fd95f
nyuguest <am7155@nyu.edu>
2019-04-27 17:18:31 -0400

    wrote init. code for ViewController

--- NYUBuzz_LocationProj/NYUBuzz_LocationProj/ViewController.swift
+++ NYUBuzz_LocationProj/NYUBuzz_LocationProj/ViewController.swift
@@ -5,21 +5,24 @@
 //  Created by nyuguest on 4/27/19.
 //  Copyright © 2019 Team RSM. All rights reserved.
 //
+// NOTE: much of this code is adapted from the following tutorial by Axel Keel
+// https://fluffy.es/current-location/
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+// SUMMARY: ViewController handles delegate mtds for locationManager
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager();  // we use locationManager to retrieve location info
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
