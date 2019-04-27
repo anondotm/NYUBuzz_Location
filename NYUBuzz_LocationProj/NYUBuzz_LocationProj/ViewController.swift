83eaeae8a05ac959f6ad03ec3fd7249b5be05264
nyuguest <am7155@nyu.edu>
2019-04-27 17:54:25 -0400

    implemented initializ location authZ request, still need to add desc to info.plst

--- NYUBuzz_LocationProj/NYUBuzz_LocationProj/ViewController.swift
+++ NYUBuzz_LocationProj/NYUBuzz_LocationProj/ViewController.swift
@@ -14,15 +14,45 @@ import CoreLocation
 // SUMMARY: ViewController handles delegate mtds for locationManager
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    @IBOutlet weak var locAuthZStatus: UILabel!
     let locationManager = CLLocationManager();  // we use locationManager to retrieve location info
 
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self;
+        // ask to updateLocAuthoriz status
+        askToEnableLoc()
+    }
+    
+    
+    
+    
+    
+    // handles asking user to enable loc services
+    func askToEnableLoc() {
+        locationManager.requestWhenInUseAuthorization();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // DELEGATE METHODS FOR LOCATIONMANAGER
+    
+    // called if loc authoriz. status is changed, updates locAuthStatus
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways:
+            locAuthZStatus.text = "authorizedAlways"
+        case .authorizedWhenInUse:
+            locAuthZStatus.text = "authorizedWhenInUse"
+        case .denied:
+            locAuthZStatus.text = "denied"
+        case .restricted:
+            locAuthZStatus.text = "restricted"
+        case .notDetermined:
+            locAuthZStatus.text = "undetermined"
+        }
     }
 }
