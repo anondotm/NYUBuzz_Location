fa4335c8e50ee9047c8a5a3fef9899990b781204
nyuguest <am7155@nyu.edu>
2019-04-27 15:27:52 -0400

    first commit! created xcode proj

---
+++ NYUBuzz_LocationProj/NYUBuzz_LocationProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYUBuzz_LocationProj
+//
+//  Created by nyuguest on 4/27/19.
+//  Copyright © 2019 Team RSM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
