c036c58958f5ad936d1bb0b014b5681e2985635b
nyuguest <am7155@nyu.edu>
2019-04-28 16:48:54 -0400

    implemented untested check-in system

--- NYUBuzz_LocationProj/NYUBuzz_LocationProj/ViewController.swift
+++ NYUBuzz_LocationProj/NYUBuzz_LocationProj/ViewController.swift
@@ -14,8 +14,19 @@ import CoreLocation
 // SUMMARY: ViewController handles delegate mtds for locationManager
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    //////////////////////////////
+    // VARIABLES
+    //////////////////////////////
+    
+    // UI outlets
     @IBOutlet weak var locAuthZStatus: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
     let locationManager = CLLocationManager();  // we use locationManager to retrieve location info
+    
+    //////////////////////////////
+    // METHODS
+    //////////////////////////////
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +35,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         askToEnableLoc()
     }
     
-    
-    
-    
+    // user presses "check-in"
+    @IBAction func userCheckIn(_ sender: Any) {
+        locationManager.requestLocation()
+    }
     
     // handles asking user to enable loc services
     func askToEnableLoc() {
@@ -38,7 +50,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // DELEGATE METHODS FOR LOCATIONMANAGER
+    //////////////////////////////
+    // DELEGATE MTDS FOR LOCMANAGER
+    //////////////////////////////
     
     // called if loc authoriz. status is changed, updates locAuthStatus
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -53,6 +67,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locAuthZStatus.text = "restricted"
         case .notDetermined:
             locAuthZStatus.text = "undetermined"
+        }
+    }
+    
+    // called once requestLocation is granted, displ. location to locationLabel
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let authZStatus = CLLocationManager.authorizationStatus();
+        
+        if (authZStatus == .authorizedAlways || authZStatus == .authorizedWhenInUse) {
+            if let location = locations.first {
+                self.locationLabel.text = "\(location.coordinate)";
+            }
+        }
+        else {
+            self.locationLabel.text = "ERROR: lacking correct locAuthZ status!"
         }
     }
 }
